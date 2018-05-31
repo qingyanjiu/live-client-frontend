c05ec05e7ce37ac317db2c3c3b9684e552d71d61
Louis <qingyanjiu@126.com>
2018-05-31 21:12:06 +0800

    added live list page

--- src/main/angular/src/app/live-list/live-list.component.ts
+++ src/main/angular/src/app/live-list/live-list.component.ts
@@ -8,9 +8,17 @@ import {DA_SERVICE_TOKEN, ITokenService} from "@delon/auth";
 })
 export class LiveListComponent implements OnInit {
 
+  lives:any[];
+
   constructor(@Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) { }
 
   ngOnInit() {
+    this.lives = [
+      {url:'http://mokulive.stream/record/147e4f5c336245db8a8d653376d96b31.png'},
+      {url:'http://mokulive.stream/record/061461c074cc4831bb81873b48a5a9b5.png'},
+      {url:'http://mokulive.stream/record/b20a9ab616fe4985b7ef46fa7401b0e2.png'},
+      {url:'http://mokulive.stream/record/2688e987ba8147bbb0e50e21db7d0fc4.png'},
+    ]
   }
 
 }
