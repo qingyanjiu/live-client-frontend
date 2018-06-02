f6704179a538015a01d905a9bd569f4b591609a0
Louis <qingyanjiu@126.com>
2018-06-02 08:58:26 +0800

    update live list

--- src/main/angular/src/app/live-list/live-list.component.ts
+++ src/main/angular/src/app/live-list/live-list.component.ts
@@ -1,8 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, ElementRef, Inject, OnInit, Renderer2} from '@angular/core';
 import {DA_SERVICE_TOKEN, ITokenService} from "@delon/auth";
 import {LiveService} from "../service/live.service";
 import {SettingsService} from "../service/settings.service";
 import {Router} from "@angular/router";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-live-list',
@@ -15,12 +16,31 @@ export class LiveListComponent implements OnInit {
 
   lives:any[];
 
-  constructor(@Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
+  colMargin = 10;
+  colWidth:number;
+
+  constructor(private el: ElementRef,
+              private renderer: Renderer2,
+              @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
               private liveService:LiveService,
               private settingsService:SettingsService,
-              private router: Router,) { }
+              private router: Router) { }
+
+  updateColWidth() {
+    let hostElem = this.el.nativeElement;
+    let liveDiv = hostElem.querySelector('.liveContent');
+    this.colWidth = (liveDiv.offsetWidth - this.colMargin * 2) / 4;
+  }
 
   ngOnInit() {
+
+    this.updateColWidth();
+    Observable.fromEvent(window, 'resize')
+      .debounceTime(100)
+      .subscribe((event) => {
+        this.updateColWidth();
+      });
+
     this.snapshoturl = this.settingsService.snapshoturl;
 
     this.liveService.getAllLives()
