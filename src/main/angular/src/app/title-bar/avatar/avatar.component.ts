b9c90341e2d5bd1e693dea8e6b9621a4e434e16d
louisliu <louisliu@cisco.com>
2018-07-03 09:55:26 +0800

    added logout modal

--- src/main/angular/src/app/title-bar/avatar/avatar.component.ts
+++ src/main/angular/src/app/title-bar/avatar/avatar.component.ts
@@ -3,6 +3,7 @@ import {DA_SERVICE_TOKEN, ITokenService} from "@delon/auth";
 import {EmitService} from "../../service/emit.service";
 import {Auth0Service} from "../../service/auth0.service";
 import {ActivatedRoute, Params} from "@angular/router";
+import {NzModalService} from "ng-zorro-antd";
 
 const ColorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
 
@@ -24,7 +25,8 @@ export class AvatarComponent implements OnInit,OnChanges {
 
   constructor(@Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
               private emitService:EmitService,
-              private authService:Auth0Service) { }
+              private authService:Auth0Service,
+              private modalService: NzModalService) { }
 
   ngOnInit() {
 
@@ -72,7 +74,20 @@ export class AvatarComponent implements OnInit,OnChanges {
   }
 
   logOut(){
-    this.authService.logout();
+    this.modalService.confirm({
+      nzTitle: 'Log Out',
+      nzContent: '<b style="color: #888888;">Are you sure to log out?</b>',
+      nzOkText: 'Yes',
+      nzOkType: 'warning',
+      nzOnOk: () => {
+        this.authService.logout();
+        this.shortName = '';
+        this.bgColor = '';
+        this.icon = 'anticon anticon-user';
+      },
+      nzCancelText: 'No',
+      nzOnCancel: () => console.log('Cancel logout')
+    });
   }
 
 }
