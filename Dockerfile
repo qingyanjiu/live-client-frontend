60ad6eefd700d0aa1346f97968655770c270843c
louisliu <louisliu@cisco.com>
2018-08-01 15:50:01 +0800

    update setting

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM node:8
 USER root
-ADD . /root/poc
+ADD . /root/web
 RUN npm -g config set user root
-WORKDIR /root/poc/src/main/angular
-RUN npm install -g @angular/cli@1.7.4
+WORKDIR /root/web/src/main/angular
 RUN npm install
 EXPOSE 4200
 CMD ng serve -H 0.0.0.0
--- src/main/angular/src/app/service/settings.service.ts
+++ src/main/angular/src/app/service/settings.service.ts
@@ -6,12 +6,12 @@ export class SettingsService {
 
   public snapshoturl = 'http://mokulive.stream/record/';
   public rtmpLiveUrl = 'rtmp://go.mokulive.stream/live/';
-  public httpflvLiveUrl = 'http://go.mokulive.stream:8880/live?app=live&stream=';
+  public httpflvLiveUrl = 'http://edge.mokulive.stream/live?app=live&stream=';
   public hlsLiveUrl = 'http://mokulive.stream/hls/';
 
   public queryUrls:any;
-  private devUrl = 'http://64.137.224.204:3000';
-  private prodUrl = 'http://64.137.224.204:3000';
+  private devUrl = 'http://test.mokulive.stream';
+  private prodUrl = 'http://test.mokulive.stream';
 
   constructor() {
 
